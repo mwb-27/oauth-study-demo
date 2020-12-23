3419c503d37c572c721cdfc6ea907823dcb620ec
wb-mwb403042@alibaba-inc.com <wb-mwb403042@alibaba-inc.com>
2020-12-23 11:17:21 +0800

    init

---
+++ index.js
@@ -0,0 +1,54 @@
+// Fill in your client ID and client secret that you obtained
+// while registering the application
+// const clientID = '7e015d8ce32370079895'
+// const clientSecret = '2b976af0e6b6ceea2b1554aa31d1fe94ea692cd9'
+
+// mwb
+const clientID = '06511b006d049de5f090'
+const clientSecret = '542d272076040f939c0bd5bc89d315efd3a0aeeb'
+const Koa = require('koa');
+const path = require('path');
+const serve = require('koa-static');
+const route = require('koa-route');
+const axios = require('axios');
+
+const app = new Koa();
+
+const main = serve(path.join(__dirname + '/public'));
+
+const oauth = async ctx => {
+  const requestToken = ctx.request.query.code;
+  console.log('authorization code:', requestToken);
+
+  const tokenResponse = await axios({
+    method: 'post',
+    url: 'https://github.com/login/oauth/access_token?' +
+      `client_id=${clientID}&` +
+      `client_secret=${clientSecret}&` +
+      `code=${requestToken}`,
+    headers: {
+      accept: 'application/json'
+    }
+  });
+
+  const accessToken = tokenResponse.data.access_token;
+  console.log(`access token: ${accessToken}`);
+
+  const result = await axios({
+    method: 'get',
+    url: `https://api.github.com/user`,
+    headers: {
+      accept: 'application/json',
+      Authorization: `token ${accessToken}`
+    }
+  });
+  console.log(result.data);
+  const name = result.data.name || result.data.login;
+
+  ctx.response.redirect(`/welcome.html?name=${name}`);
+};
+
+app.use(main);
+app.use(route.get('/oauth/redirect', oauth));
+
+app.listen(8080);
